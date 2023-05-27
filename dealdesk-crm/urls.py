12c6a6544d1bc9c3d5cf76cd80b0bb74d40bc4dc
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-05-27 13:24:45 +0000

    Bug-fix: change project name to align with herokuapp name

--- dealdesk-crm/urls.py
+++ dealdesk-crm/urls.py
@@ -1,4 +1,4 @@
-"""crm URL Configuration
+"""dealdesk-crm URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.2/topics/http/urls/
