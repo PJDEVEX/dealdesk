041cc83be734e1f86eacc94fd0d565679efd2c32
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-05-29 19:17:11 +0000

    Register all models in the admin site

--- client/admin.py
+++ client/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.apps import apps  # Import the apps module from Django
 
-# Register your models here.
+# The apps module provides tools for working with Django applications
+# It allows access to various application configurations and models
+
+
+all_models = apps.get_models()
+
+for model in all_models:
+    try:
+        admin.site.register(model)  # Register the model in the admin site
+    except admin.sites.AlreadyRegistered:
+        pass  # If the model is already registered, skip it
