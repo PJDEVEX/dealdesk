cfbb61a41fca9cfb85a16877fd709a81be2b70b3
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-05-29 19:47:14 +0000

    Comment: add comment to highlight the registration of all models

--- client/admin.py
+++ client/admin.py
@@ -3,7 +3,7 @@ from django.apps import apps  # Import the apps module from Django
 
 # The apps module provides tools for working with Django applications
 # It allows access to various application configurations and models
-
+# Here, I register models of all the apps
 
 all_models = apps.get_models()
 
