9ee982644f1a58468435755adb3f701c770dd42b
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-06-03 13:59:14 +0000

    Register taskmanager model in admin view

--- task_manager/admin.py
+++ task_manager/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from task_manager.models import TaskManager
+from team.models import Sar
 
 # Register TaskManager model
-admin.site.register(TaskManager)
+
+
+@admin.register(TaskManager)
+class TaskManagerAdmin(admin.ModelAdmin):
+    # Define the list of fields to be displayed in the list view
+    list_display = (
+        'id',
+        'title',
+        'due_date',
+        'status',
+        'priority',
+        'assigned_to',
+        )
+    # Define the list of fields to be used for filtering the change list view
+    list_filter = (
+        'status',
+        'priority',
+        )
