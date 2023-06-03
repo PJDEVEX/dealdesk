3fee3d6d208576b045a68fed47fbf77023c6d0a9
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-06-03 13:10:26 +0000

    Register the models in admin view

--- project/admin.py
+++ project/admin.py
@@ -1,21 +1,64 @@
 from django.contrib import admin
 from project.models import Project, Category, Brand
-# from django.apps import apps  # Import the apps module from Django
+from team.models import Sar
 
-# # The apps module provides tools for working with Django applications
-# # It allows access to various application configurations and models
-# # Here, I register models of all the apps
 
-# all_models = apps.get_models()
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    # Define the list of fields to be displayed in the change list view
+    list_display = (
+        'id',
+        'name',
+        'client',
+        'location',
+        'brand',
+        'category',
+        'type_of_construction',
+        'project_status',
+        'est_closing_date',        
+        'est_date_of_delivery',
+        'potential_value',
+        'winning_chance',
+        'forecast_pxp',
+        'salesman',
+        )
+    # Define the list of fields to be used for filtering the change list view
+    list_filter = (
+        'project_status',
+        'type_of_construction',
+        'winning_chance',
+        'brand',
+        'category',
+        'salesman',
+        )
+    # Search fields allow searching by project name, location, brand, category,
+    # salesman, or client company name.
+    search_fields = [
+        'name',
+        'location',
+        'brand__brand',
+        'category__category',
+        'salesman__first_name',
+        'salesman__last_name',
+        'client__company_name',
+        ]
+    # Add a helper text so that user finds it easy to search
+    change_list_template = 'admin/project/project_change_list.html'
 
-# for model in all_models:
-#     try:
-#         admin.site.register(model)  # Register the model in the admin site
-#     except admin.sites.AlreadyRegistered:
-#         pass  # If the model is already registered, skip it
 
-# Register the Project, Brand, Category Models in the admin site
-models_to_register = [Project, Brand, Category]
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    # Define the list of fields to be displayed in the change list view
+    list_display = (
+        'id',
+        'brand',
+        )
 
-for model in models_to_register:
-    admin.site.register(model)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    # Define the list of fields to be displayed in the change list view
+    list_display = (
+        'id',
+        'category',
+        )
