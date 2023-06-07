f1ef1e39954ddca037063b4004fdb6a4ec8076a8
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-06-07 23:13:41 +0000

    Bug fix: remove light-dark.css

--- dealdesk-crm/settings.py
+++ dealdesk-crm/settings.py
@@ -37,7 +37,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     'dealdesk-crm.herokuapp.com',
     'localhost',
-    '8000-pjdevex-dealdesk-tf1tnnys1kc.ws-eu98.gitpod.io',
+    '8000-pjdevex-dealdesk-x2u1gsmdgdp.ws-eu99.gitpod.io',
     '127.0.0.1',
     ]
 
