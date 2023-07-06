8e1aa42de66adc6942c79019a1d12e2a811886cb
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-07-06 20:14:44 +0000

    Refactor str method in ModelName to display first name

--- team/models.py
+++ team/models.py
@@ -21,7 +21,7 @@ class Manager(models.Model):
         ordering = ['first_name']
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name}"
 
 
 class Sar(models.Model):
@@ -42,4 +42,4 @@ class Sar(models.Model):
         verbose_name_plural = 'Sars'
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.first_name}"
