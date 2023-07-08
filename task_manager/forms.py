bf0d72330b2b01044f571e4efa45cc64a7d03af5
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-07-08 14:48:42 +0000

    Refactoing task list view

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,13 +1,33 @@
 from django import forms
-from django.forms import DateInput, TimeInput
-from django.utils import timezone
+from django.forms import DateInput
 from .models import TaskManager, STATUS_CHOICES, PRIORITY_CHOICES
 from team.models import Sar
 
 
+class TaskManagerFilterForm(forms.Form):
+    """
+    Filter form for filtering task managers
+    """
+    assigned_to = forms.ModelChoiceField(
+        queryset=Sar.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False
+    )
+    status = forms.ChoiceField(
+        choices=[('', 'All')] + STATUS_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False
+    )
+    priority = forms.ChoiceField(
+        choices=[('', 'All')] + PRIORITY_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        required=False
+    )
+
+
 class TaskManagerForm(forms.ModelForm):
     """
-    Form for creating and updating task information.
+    Form for creating and updating task manager information.
     """
     class Meta:
         model = TaskManager
@@ -21,47 +41,14 @@ class TaskManagerForm(forms.ModelForm):
             'assigned_to': 'Assigned To',
         }
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title', 'tabindex': '1'}),
-            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter description', 'tabindex': '2'}),
-            'due_date': DateInput(attrs={'class': 'vDateField form-control', 'required': '', 'id': 'id_due_date_0'}),
-            'due_time': TimeInput(attrs={'class': 'vTimeField form-control', 'required': '', 'id': 'id_due_date_1'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title', 'tabindex': '1'}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Description', 'tabindex': '2'}),
+            'due_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date', 'tabindex': '3'}),
             'status': forms.Select(attrs={'class': 'form-control', 'tabindex': '4'}),
             'priority': forms.Select(attrs={'class': 'form-control', 'tabindex': '5'}),
             'assigned_to': forms.Select(attrs={'class': 'form-control', 'tabindex': '6'}),
         }
 
-    def clean_due_date(self):
-        """
-        Validate the due date to ensure it is in the future.
-        """
-        due_date = self.cleaned_data['due_date']
-        due_time = self.cleaned_data['due_time']
-        due_datetime = timezone.datetime.combine(due_date, due_time)
-        if due_datetime < timezone.now():
-            raise forms.ValidationError("Due date and time must be in the future.")
-        return due_datetime
-
-
-class TaskManagerFilterForm(forms.Form):
-    """
-    Filter form for filtering tasks.
-    """
-    status = forms.ChoiceField(
-        choices=[('', 'All')] + STATUS_CHOICES,
-        widget=forms.Select(attrs={'class': 'form-control', 'tabindex': '1'}),
-        required=False
-    )
-    priority = forms.ChoiceField(
-        choices=[('', 'All')] + PRIORITY_CHOICES,
-        widget=forms.Select(attrs={'class': 'form-control', 'tabindex': '2'}),
-        required=False
-    )
-    assigned_to = forms.ModelChoiceField(
-        queryset=Sar.objects.all(),
-        widget=forms.Select(attrs={'class': 'form-control', 'tabindex': '3'}),
-        required=False
-    )
-    search_keyword = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Search tasks', 'tabindex': '4'}),
-        required=False
-    )
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['assigned_to'].queryset = Sar.objects.all()
--- task_manager/models.py
+++ task_manager/models.py
@@ -23,7 +23,7 @@ PRIORITY_CHOICES = [
 class TaskManager(models.Model):
     title = models.CharField(max_length=55, null=False)
     description = models.CharField(max_length=255)
-    due_date = models.DateTimeField(null=False)
+    due_date = models.DateField(null=False)
     status = models.CharField(
         max_length=50,
         choices=STATUS_CHOICES,
