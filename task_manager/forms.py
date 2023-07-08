b54b4c90bb9fa6f9ca2f884622b834a919e0e1a6
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-07-08 12:13:43 +0000

    Refactor task_list view

--- task_manager/forms.py
+++ task_manager/forms.py
@@ -1,35 +1,14 @@
 from django import forms
-from django.forms import DateTimeInput
+from django.forms import DateInput, TimeInput
+from django.utils import timezone
 from .models import TaskManager, STATUS_CHOICES, PRIORITY_CHOICES
 from team.models import Sar
 
 
-class TaskManagerFilterForm(forms.Form):
-    """
-    Filter form for filtering tasks in the Task Manager.
-    """
-    status = forms.ChoiceField(
-        choices=[('', 'All')] + list(STATUS_CHOICES),
-        widget=forms.Select(attrs={'class': 'form-control'}),
-        required=False
-    )
-    priority = forms.ChoiceField(
-        choices=[('', 'All')] + list(PRIORITY_CHOICES),
-        widget=forms.Select(attrs={'class': 'form-control'}),
-        required=False
-    )
-    assigned_to = forms.ModelChoiceField(
-        queryset=Sar.objects.all(),
-        widget=forms.Select(attrs={'class': 'form-control'}),
-        required=False
-    )
-
-
 class TaskManagerForm(forms.ModelForm):
     """
-    Form for creating and updating tasks in the Task Manager.
+    Form for creating and updating task information.
     """
-
     class Meta:
         model = TaskManager
         fields = '__all__'
@@ -42,14 +21,47 @@ class TaskManagerForm(forms.ModelForm):
             'assigned_to': 'Assigned To',
         }
         widgets = {
-            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title', 'tabindex': '1'}),
-    'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Description', 'tabindex': '2'}),
-    'due_date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime', 'tabindex': '3'}),
-    'status': forms.Select(attrs={'class': 'form-control', 'tabindex': '4'}),
-    'priority': forms.Select(attrs={'class': 'form-control', 'tabindex': '5'}),
-    'assigned_to': forms.Select(attrs={'class': 'form-control', 'tabindex': '6'}),
+            'title': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter title', 'tabindex': '1'}),
+            'description': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter description', 'tabindex': '2'}),
+            'due_date': DateInput(attrs={'class': 'vDateField form-control', 'required': '', 'id': 'id_due_date_0'}),
+            'due_time': TimeInput(attrs={'class': 'vTimeField form-control', 'required': '', 'id': 'id_due_date_1'}),
+            'status': forms.Select(attrs={'class': 'form-control', 'tabindex': '4'}),
+            'priority': forms.Select(attrs={'class': 'form-control', 'tabindex': '5'}),
+            'assigned_to': forms.Select(attrs={'class': 'form-control', 'tabindex': '6'}),
         }
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.fields['assigned_to'].queryset = Sar.objects.all()
+    def clean_due_date(self):
+        """
+        Validate the due date to ensure it is in the future.
+        """
+        due_date = self.cleaned_data['due_date']
+        due_time = self.cleaned_data['due_time']
+        due_datetime = timezone.datetime.combine(due_date, due_time)
+        if due_datetime < timezone.now():
+            raise forms.ValidationError("Due date and time must be in the future.")
+        return due_datetime
+
+
+class TaskManagerFilterForm(forms.Form):
+    """
+    Filter form for filtering tasks.
+    """
+    status = forms.ChoiceField(
+        choices=[('', 'All')] + STATUS_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control', 'tabindex': '1'}),
+        required=False
+    )
+    priority = forms.ChoiceField(
+        choices=[('', 'All')] + PRIORITY_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control', 'tabindex': '2'}),
+        required=False
+    )
+    assigned_to = forms.ModelChoiceField(
+        queryset=Sar.objects.all(),
+        widget=forms.Select(attrs={'class': 'form-control', 'tabindex': '3'}),
+        required=False
+    )
+    search_keyword = forms.CharField(
+        widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Search tasks', 'tabindex': '4'}),
+        required=False
+    )
--- task_manager/views.py
+++ task_manager/views.py
@@ -1,6 +1,8 @@
-from django.views.generic import ListView
+from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView
 from .models import TaskManager
-from .forms import TaskManagerFilterForm
+from .forms import TaskManagerFilterForm, TaskManagerForm
 
 
 class TaskListView(ListView):
@@ -37,3 +39,19 @@ class TaskListView(ListView):
         context = super().get_context_data(**kwargs)
         context['filter_form'] = TaskManagerFilterForm(self.request.GET)
         return context
+
+
+class TaskCreateView(CreateView):
+    model = TaskManager
+    form_class = TaskManagerForm
+    template_name = 'task_manager/task_create.html'
+    success_url = reverse_lazy('task_manager:task_list')
+
+    def form_invalid(self, form):
+        # Add styling to invalid fields
+        for field in form.errors:
+            form[field].field.widget.attrs['class'] += ' is-invalid'
+        return self.render_to_response(self.get_context_data(form=form))
+
+    def form_valid(self, form):
+        return super().form_valid(form)
