e3271b2feb81e685673bde11714819d8d76d133f
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-07-08 15:13:29 +0000

    Refactoing task create view

--- task_manager/urls.py
+++ task_manager/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import TaskListView
+from .views import TaskListView, TaskCreateView
 from django.urls import include
 
 app_name = 'task_manager'
 
 urlpatterns = [
     path('list/', TaskListView.as_view(), name='task_list'),
-    # path('new/', TaskCreateView.as_view(), name='task_new')
+    path('new/', TaskCreateView.as_view(), name='task_new')
 ]
